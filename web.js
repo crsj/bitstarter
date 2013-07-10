7defd0b3551f26d3b90c19716443fc0b97e47622
Ubuntu <ubuntu@ip-10-243-85-136.ec2.internal>
2013-07-10 22:59:18 +0000

    updated web.js to retrun index.html

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
+
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+	var indexfile = "/home/ubuntu/week3/hw3ProgAssn/bitstarter/index.html";
+	var indexdata = fs.readFileSync(indexfile) + ' ';
+	//console.log("data " + __filename + "\n Wrote: " + indexdata);
+  response.send(indexdata);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
